f48426fa7aac8289ca714d720d3a13e23cbe16c4
vrk <victoriakirst@gmail.com>
2024-01-22 10:56:13 -0500

    add files

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images/");
   eleventyConfig.addPassthroughCopy("scripts/");
   eleventyConfig.addPassthroughCopy("css/");
+  eleventyConfig.addPassthroughCopy("js/");
   eleventyConfig.addPassthroughCopy("fonts/");
 };
---
+++ contribute-journal.md
@@ -0,0 +1,6 @@
+---
+layout: main
+title: Submit a Journal Layout — Pouch Cafe
+stylesheet: /css/about.css
+script: /js/journal.js
+---
